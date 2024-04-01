c425156bf67786d855bd21f83efac78bcc933044
Nikhil Mudgal <nikhilm2097@gmail.com>
2024-04-01 23:31:00 +0530

    Setup Route and inital setup of file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { Routes, Route } from 'react-router-dom';
 import './globals.css';
+import { Home } from './_root/pages';
+import SignInForm from './_auth/forms/SignInForm';
+import SignUpForm from './_auth/forms/SignUpForm';
+import AuthLayout from './_auth/forms/AuthLayout';
+import RootLayout from './_root/pages/RootLayout';
 const App = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+       <main className='flex h-screen'>
+        <Routes>
+          {/* public routes */}
+          <Route element={<AuthLayout />}>
+          <Route path="/sign-in"  element={<SignInForm />}/>
+          <Route path="/sign-up"  element={<SignUpForm />}/>
+          </Route>
+
+          {/* private routes */}
+          <Route element={<RootLayout />} >
+          <Route index  element={<Home />}/>
+          </Route>
+        </Routes>
+        </main>
     )
 }
 
